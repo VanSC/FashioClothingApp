2eb4eb8cbad529b30e48f50b78ac95f9cf4136e4
Vanessa <vanessa.choque@tecsup.edu.pe>
2023-10-20 13:17:31 -0500

    Initial Commit

---
+++ FashionCatalogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FashionCatalogApp
+//
+//  Created by MAC45 on 20/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
